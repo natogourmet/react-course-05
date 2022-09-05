c30c22a19a73c3fb506e9a7c04849f25a57888c8
natogourmet <natogourmet@hotmail.com>
2022-09-04 23:38:29 -0500

    useState destructuration and spread

---
+++ src/01-useState/CounterApp.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export const CounterApp = () => {
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  const { counter1, counter2, counter3 } = state;
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <hr />
+      <h2>{counter1}</h2>
+      <h2>{counter2}</h2>
+      <h2>{counter3}</h2>
+      <button
+        className="btn"
+        onClick={() =>
+          setCounter({
+            ...state,
+            counter1: counter1 + 1,
+          })
+        }
+      >
+        +1
+      </button>
+    </>
+  );
+};
