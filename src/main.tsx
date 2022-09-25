ec77f0fba28ebbbbae3c889d5f2cc0e79063bc2b
natogourmet <natogourmet@hotmail.com>
2022-09-25 18:42:37 -0500

    removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './01-useState/CounterApp';
-import { HooksApp } from './HooksApp';
+// import { CounterApp } from './01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
 import './index.css';
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHoks } from './03-examples/MultipleCustomHoks';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <FormWithCustomHook />
-  </React.StrictMode>
+  <>
+    <MultipleCustomHoks />
+  </>
 );
