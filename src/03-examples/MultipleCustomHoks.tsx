804c6042f1ad455ce05184aaf08a524640596332
natogourmet <natogourmet@hotmail.com>
2022-09-25 19:16:28 -0500

    useCount in multiple hooks component

--- src/03-examples/MultipleCustomHoks.tsx
+++ src/03-examples/MultipleCustomHoks.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import { useFetch } from '../hooks/useFetch';
+import { useCounter } from '../hooks/useCounter';
+
 
 export const MultipleCustomHoks = () => {
-  const fetch = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+  const { counter, increment } = useCounter(1);
 
-  console.log(fetch);
+  const { data, loading, hasError } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
+
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-0">{quote}</p>
+          <footer className="blockquote-footer"> {author} </footer>
+        </blockquote>
+      )}
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          increment(1);
+        }}
+      >
+        Next quote
+      </button>
     </>
   );
 };
